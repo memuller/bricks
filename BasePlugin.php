e7c8bd02ed3387f9869619a28d70dbcfa62f1048
Matheus E. Muller <hello@memuller.com>
2013-05-24 22:11:03 -0300

    Builds taxonomies first.

--- BasePlugin.php
+++ BasePlugin.php
@@ -30,7 +30,7 @@
 
 		static function build(){
 			$base = get_called_class(); $namespace = '\\'.get_namespace($base) . '\\';
-			foreach (array_merge(static::$custom_classes, static::$custom_posts, static::$custom_singles, static::$custom_taxonomies, static::$custom_users) as $object) {
+			foreach (array_merge(static::$custom_taxonomies, static::$custom_classes, static::$custom_posts, static::$custom_singles, static::$custom_users) as $object) {
 				require( static::path('models/'. $object . '.php'));
 				$class = $namespace. ucfirst($object);
 				$class::build();
