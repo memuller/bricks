e1956716275df9b5b132fba6bce0d0ec263d2f3d
Matheus E. Muller <hello@memuller.com>
2013-08-19 01:05:58 -0300

    Creates setters for customposts content & title.

--- BasePost.php
+++ BasePost.php
@@ -5,6 +5,15 @@
 		static $taxonomies = array();
 		public $unfiltered_fields = array();
 		static $meta_type = 'post';
-	}
-		
+	
+		function __set($name, $value){
+			if('content' == $name){
+				wp_update_post( array('ID' => $this->ID, 'post_content' => $value) );
+			} elseif('title' == $name) {
+				wp_update_post( array('ID' => $this->ID, 'post_title' => $value) );
+			}else {
+				parent::__set($name, $value);
+			}
+		}
+	}	
 ?>
