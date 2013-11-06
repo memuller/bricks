9f44730a5f5951855134570be2179efb35918469
Matheus E. Muller <hello@memuller.com>
2013-11-06 18:31:31 -0200

    Bugfixes media fields.

--- views/admin/fields/media.php
+++ views/admin/fields/media.php
@@ -1,3 +1,3 @@
 <?php $field = $object->$field;?>
-<input type="hidden" <?php html_attributes(array('name' => $name, 'id' => $id)) ?>>
+<input type="hidden" <?php html_attributes(array('name' => $name, 'id' => $id, 'value' => $field)) ?>>
 <input type="button" class="upload button media" value='<?php echo $field ? explode('/', $field)[sizeof(explode('/', $field))-1] : 'select...' ?>' id="<?php echo $id.'-button' ?>" style="margin-left: 20px;">
