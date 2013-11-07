fb83a720bf59da128ab6b0681372577360d19c98
Matheus E. Muller <hello@memuller.com>
2013-11-07 03:16:41 -0200

    Bugfixes issue w/ media fields filenames.

--- views/admin/fields/media.php
+++ views/admin/fields/media.php
@@ -1,3 +1,3 @@
 <?php $field = $object->$field; $filename = explode('/', $field); $filename = $filename[sizeof($filename)-1] ; ?>
 <input type="hidden" <?php html_attributes(array('name' => $name, 'id' => $id, 'value' => $field)) ?>>
-<input type="button" class="upload button media" value='<?php echo $field ?  : 'select...' ?>' id="<?php echo $id.'-button' ?>" style="margin-left: 20px;">
+<input type="button" class="upload button media" value='<?php echo $field ? $filename : 'select...' ?>' id="<?php echo $id.'-button' ?>" style="margin-left: 20px;">
