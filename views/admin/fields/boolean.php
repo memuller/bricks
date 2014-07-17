4c8ac6f74fac77e7bec610ede0758d5cb08c6d68
Matheus E. Muller <hello@memuller.com>
2014-07-17 19:00:36 -0300

    Handles default values on boolean fields.

--- views/admin/fields/boolean.php
+++ views/admin/fields/boolean.php
@@ -1,3 +1,4 @@
+<?php $screen = get_current_screen(); $value = $screen->action == 'add' ? $options['default'] : $object->$field ;?>
 <input type="checkbox" <?php html_attributes( array( 
 	'name' => $name, 'id' => $id, 'class' => 'text', 'value' => 1 
-	)) ?> <?php echo $object->$field ? 'checked' : '' ?> <?php echo $html ?> <?php echo $validations ?> > 
+	)) ?> <?php echo $value ? 'checked' : '' ?> <?php echo $html ?> <?php echo $validations ?> > 
