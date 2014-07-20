8967214c29ff73655373411869e76b6506b5bead
Matheus E. Muller <hello@memuller.com>
2014-07-20 13:46:11 -0300

    Fixes variable naming error.

--- views/admin/fields/editor.php
+++ views/admin/fields/editor.php
@@ -1,4 +1,4 @@
-<?php wp_editor($object->$field, $id, array('name' => $name, 'textarea_rows' => 15, 'drag_drop_upload' => true, 
+<?php wp_editor($object->$field, $id, array('textarea_name' => $name, 'textarea_rows' => 15, 'drag_drop_upload' => true, 
 	'media_buttons' => isset($options['media_buttons']) && !$options['media_buttons'] ? false : true,
 	'teeny' => isset($options['teeny']) && $options['teeny'] ? true : false
 )) ;?>
