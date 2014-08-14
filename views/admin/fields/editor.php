6d693dd72f9250644409dece60f094fb1c1909ab
Matheus E. Muller <hello@memuller.com>
2014-08-14 15:30:14 -0300

    Implements variable height on editor fields (finally).

--- views/admin/fields/editor.php
+++ views/admin/fields/editor.php
@@ -1,4 +1,6 @@
-<?php wp_editor($object->$field, $id, array('textarea_name' => $name, 'textarea_rows' => 15, 'drag_drop_upload' => true, 
+<?php wp_editor($object->$field, $id, array('textarea_name' => $name, 'drag_drop_upload' => true, 
 	'media_buttons' => isset($options['media_buttons']) && !$options['media_buttons'] ? false : true,
-	'teeny' => isset($options['teeny']) && $options['teeny'] ? true : false
+	'teeny' => isset($options['teeny']) && $options['teeny'] ? true : false,
+	'wpautop' => !isset($options['wpautop']) || !$options['wpautop'] ? false : true,
+	'tinymce' => array('height' => !isset($options['height']) ? 300 : $options['height'])
 )) ;?>
