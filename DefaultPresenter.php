d4addc79c9fe6e8e279e60e3d996633e005121d0
Matheus E. Muller <hello@memuller.com>
2016-10-02 20:43:06 -0300

    Makes DefaultPresenter load bundled assets.

--- DefaultPresenter.php
+++ DefaultPresenter.php
@@ -1,5 +1,26 @@
 <?php
 	class DefaultPresenter extends Presenter{
 
+		static $includes = [];
+
+		static function build(){
+			$plugin = static::$namespace . 'Plugin';
+			$class = get_called_class();
+			foreach (['scripts', 'styles'] as $resource) {
+				$version_var = "${resource}_version";
+				$extension = $resource == 'scripts' ? 'js' : 'css';
+
+				if($plugin::$$version_var){
+					$class::$$resource = [
+						'main' => [ 'source' => "/assets/dist/bundle.${extension}",
+							'from' => 'plugin', 'version' => $plugin::$$version_var ]
+					];
+					$class::$includes[]= [
+						'is' => 'any', $resource => ['main']
+					];
+				}
+			}
+			parent::build();
+		}
 	}
 ?>
