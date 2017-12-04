87e6329c5946dfecb5796282859cc335d2d8333d
Matheus E. Muller <hello@memuller.com>
2017-12-04 01:19:32 -0200

    Adds autoloader and basic CustomPost skeleton

---
+++ src/CustomPost.php
@@ -0,0 +1,23 @@
+<?php 
+namespace Bricks;
+class CustomPost {
+  static $fields = array();
+  static $boxes = array();
+  static $name;
+  static $creation_parameters;
+
+  static function build(){
+    $klass = $get_called_class();
+
+  }
+
+  static function create_post_type(){
+
+  }
+
+  static function create_metaboxes(){
+    
+  }
+}
+
+?>
