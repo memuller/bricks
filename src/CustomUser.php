184f5fa871381920a704172b27fdf3782eba8ee8
Matheus E. Muller <hello@memuller.com>
2017-12-05 19:01:36 -0200

    Minor improvements on CustomUser

--- src/CustomUser.php
+++ src/CustomUser.php
@@ -10,15 +10,17 @@ class CustomUser extends BaseItem {
   static function prepare_parameters(){
     $klass = get_called_class();
     $boxes = array();
+    # shows boxes only if an user belonging to this class is being edited
     foreach(static::$boxes as $id => $box){ 
       $box['show_on_cb'] = function() use($klass) {
-        return static::user_is(static::edit_user());
+        return static::user_belongs(static::get_currently_edited_user());
       };
       $boxes[$id] = $box;
     }
     static::$boxes = $boxes;
   }
 
+  # TODO: if WP_DEBUG always create; otherwise create only if not exists
   static function create_content_type(){
     remove_role(static::$name);
     $inherits_from = get_role( static::$inherits_from ); 
@@ -38,15 +40,18 @@ class CustomUser extends BaseItem {
   static function is_default(){
   }
 
-  static function edit_user(){
+  # gets the user currently being edited in the admin
+  static function get_currently_edited_user(){
+    # if IS_PROFILE_PAGE, user is editing itself
     if(defined('IS_PROFILE_PAGE') && IS_PROFILE_PAGE){
       return wp_get_current_user();
-    } else {
+    } else { #otherwise there's always an user_id as GET param
       return get_user_by('ID', $_GET['user_id']);
     }
   }
 
-  static function user_is($user){
+  # returns true if the given user belongs to this class
+  static function user_belongs($user){
     return  in_array(static::$name, $user->roles) || 
             (static::$allow_admin && in_array('administrator', $user->roles));
   }
