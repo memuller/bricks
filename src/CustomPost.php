2f26609c4f571f749209ba53c93a3931af310aad
Matheus E. Muller <hello@memuller.com>
2018-01-19 15:13:40 +0000

    Handles edit-views admin hook

--- src/CustomPost.php
+++ src/CustomPost.php
@@ -1,18 +1,18 @@
-<?php 
+<?php
 namespace Bricks;
 class CustomPost extends BaseItem {
-  
+
   static  $content_type = 'post';
   static  $public = true,
           $labels, $supports, $taxonomies,
           $description, $show_ui, $menu_position, $menu_icon,
-          $hierarchical = false, $show_in_rest = true, $capability_type ;
+          $hierarchical = false, $show_in_rest = true, $capability_type, $capabilities, $map_meta_cap, $hide_add = false ;
 
-  
+
   static function prepare_parameters(){
     $klass = get_called_class(); $params = array();
     # sets post registration parameters
-    foreach(['label', 'labels', 'public', 'supports', 'taxonomies', 'description', 'show_ui', 'menu_position', 'menu_icon', 'hierarchical', 'capability_type', 'show_in_rest'] as $arg){
+    foreach(['label', 'labels', 'public', 'supports', 'taxonomies', 'description', 'show_ui', 'menu_position', 'menu_icon', 'hierarchical', 'capability_type', 'show_in_rest', 'capabilities', 'map_meta_cap'] as $arg){
       if(isset($klass::$$arg)){
         $params[$arg] = $klass::$$arg;
       }
@@ -20,7 +20,7 @@ class CustomPost extends BaseItem {
     $klass::$creation_parameters =& $params;
   }
 
-  
+
   static function create_content_type(){
     $klass = get_called_class();
     add_action('init', function() use($klass) {
@@ -45,6 +45,7 @@ class CustomPost extends BaseItem {
     # aliases for some post values
     if('title' == $thing) $thing = 'post_title';
     if('content' == $thing) $thing = 'post_content';
+    if('status' == $thing) $thing = 'post_status';
     if('id' == $thing) $thing = 'ID';
     # returns a custom field or a post attribute
     return parent::__get($thing);
@@ -71,6 +72,23 @@ class CustomPost extends BaseItem {
     }, $posts);
   }
 
+  static function setup_hooks(){
+    $class = get_called_class();
+
+    if (is_callable([$class, 'on_views_edit'])){
+      add_filter("views_edit-".static::name(), [$class, 'on_views_edit']);
+    }
+    
+    if (is_callable([$class, 'on_save'])){
+      add_action('save_post', function($post_id, $post, $update) use ($class) {
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return ;
+        if ($class::name() != $post->post_type) return ;
+        $obj = new $class($post_id);
+        $class::on_save($obj, $update);
+      }, 10, 3);
+    }
+  }
+
 }
 
-?>  
+?>
