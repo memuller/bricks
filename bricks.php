4bcaded78fee0ecfdc3c2ef437b41dc4b3699b61
Matheus E. Muller <hello@memuller.com>
2018-01-22 13:11:24 +0000

    Adds class autoloader

--- bricks.php
+++ bricks.php
@@ -31,4 +31,25 @@
 	function loopable($arg){
 		return is_array($arg) ? $arg : [$arg];
 	}
+
+
+	spl_autoload_register(function(string $class){
+    $class_path = explode('\\', $class);
+    $class_name = $class_path[sizeof($class_path)-1];
+    $namespace = $class_path[0];
+    if(BRICKS_NAMESPACE == $namespace){
+			$path = implode(DIRECTORY_SEPARATOR, [BRICKS_BASE_DIR, 'models', "$class_name.php"]);
+			if (!file_exists($path)) return;
+      require_once $path;
+    }
+	});
+	
+  foreach(glob(implode(DIRECTORY_SEPARATOR, [BRICKS_BASE_DIR, 'models', '*.php'])) as $file){
+    $path = explode(DIRECTORY_SEPARATOR, $file);
+    $class_name =  explode('.', $path[sizeof($path)-1])[0] ;
+    $class = BRICKS_NAMESPACE.'\\'.$class_name;
+    $class::init();
+  }
+	
+
 ?>
