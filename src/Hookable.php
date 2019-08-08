aead2e8a3f5ed76cda681d45225c12e0f4c02d88
Matheus E. Muller <hello@memuller.com>
2019-08-07 22:32:09 -0300

    Supports action arrays on Hookable

--- src/Hookable.php
+++ src/Hookable.php
@@ -13,10 +13,18 @@ trait Hookable
   {
     if (isset(static::$actions) && !empty(static::$actions)) {
       foreach (static::$actions as $action => $args) {
-        is_array($args) || $args = [$args];
-        isset($args[1]) || $args[1] = 10;
-        isset($args[2]) || $args[2] = 0;
-        static::add_action($action, $args[0], $args[1], $args[2]);
+        $is_single = ! is_array($args);
+        $is_one_size_array = is_array($args) && sizeof($args) === 1;
+        $is_params = is_array($args) && isset($args[1]) && is_int($args[1]);
+        $not_many = $is_single || $is_one_size_array || $is_params;
+        if ($not_many)
+          $args = [$args];
+        foreach ($args as $item){
+          is_array($item) || $item = [$item];
+          isset($item[1]) || $item[1] = 10;
+          isset($item[2]) || $item[2] = 0;
+          static::add_action($action, $item[0], $item[1], $item[2]);
+        }
       }
     }
 
